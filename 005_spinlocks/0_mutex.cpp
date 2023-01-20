71e2d61de8a98343c81b311e149f11ac05888ba4
Nick <coffeebeforearch@gmail.com>
2023-01-20 17:10:13 -0600

    Update spinlock examples

--- 005_spinlocks/0_mutex.cpp
+++ 005_spinlocks/0_mutex.cpp
@@ -1,43 +1,46 @@
-#include <mutex>
-#include <chrono>
-#include <thread>
-#include <random>
-#include <list>
+// A simple example using a mutex
+// By: Nick from CoffeeBeforeArch
+
 #include <algorithm>
+#include <chrono>
+#include <list>
+#include <mutex>
+#include <random>
+#include <thread>
 
 int main() {
-    // Random number generate
-    std::random_device rd;
-    std::mt19937 mt(rd());
-    std::uniform_int_distribution dist(10, 20);
+  // Random number generate
+  std::random_device rd;
+  std::mt19937 mt(rd());
+  std::uniform_int_distribution dist(10, 20);
 
-    // Vector of random numbers
-    std::list<int> l;
-    std::generate_n(std::back_inserter(l), 1<<20, [&](){return dist(mt);});
+  // Vector of random numbers
+  std::list<int> l;
+  std::generate_n(std::back_inserter(l), 1 << 20, [&]() { return dist(mt); });
 
-    // Our lock guarding access to the list
-    std::mutex m;
+  // Our lock guarding access to the list
+  std::mutex m;
 
-    // Function that removes items from the list in parallel
-    auto work = [&](){
-        // Run until there are no more items
-        while(true) {
-            // Grab the lock before doing anything
-            std::lock_guard<std::mutex> lg(m);
+  // Function that removes items from the list in parallel
+  auto work = [&]() {
+    // Run until there are no more items
+    while (true) {
+      // Grab the lock before doing anything
+      std::lock_guard<std::mutex> lg(m);
 
-            // Check if there are no more items
-            if (l.empty()) break;
+      // Check if there are no more items
+      if (l.empty()) break;
 
-            // Remove the item at the end of the list
-            l.pop_back();
-        }
-    };
-
-    // Spawn threads
-    int num_threads = 8;
-    std::vector<std::jthread> threads;
-    for(int i = 0; i < num_threads; i++) {
-        threads.emplace_back(work);
+      // Remove the item at the end of the list
+      l.pop_back();
     }
-    return 0;
+  };
+
+  // Spawn threads
+  int num_threads = 8;
+  std::vector<std::jthread> threads;
+  for (int i = 0; i < num_threads; i++) {
+    threads.emplace_back(work);
+  }
+  return 0;
 }
--- 005_spinlocks/1_spinlock.cpp
+++ 005_spinlocks/1_spinlock.cpp
@@ -1,48 +1,52 @@
-#include <mutex>
-#include <chrono>
-#include <thread>
-#include <random>
-#include <list>
-#include <algorithm>
+// A simple example using a spinlock
+// By:Nick from CoffeeBeforeArch
+
 #include <pthread.h>
 
+#include <algorithm>
+#include <chrono>
+#include <list>
+#include <mutex>
+#include <random>
+#include <thread>
+
 int main() {
-    // Random number generate
-    std::random_device rd;
-    std::mt19937 mt(rd());
-    std::uniform_int_distribution dist(10, 20);
+  // Random number generate
+  std::random_device rd;
+  std::mt19937 mt(rd());
+  std::uniform_int_distribution dist(10, 20);
 
-    // Vector of random numbers
-    std::list<int> l;
-    std::generate_n(std::back_inserter(l), 1<<20, [&](){return dist(mt);});
+  // Vector of random numbers
+  std::list<int> l;
+  std::generate_n(std::back_inserter(l), 1 << 20, [&]() { return dist(mt); });
 
-    // Our lock guarding access to the list
-    pthread_spinlock_t spinlock;
-    pthread_spin_init(&spinlock, 0);
+  // Our lock guarding access to the list
+  pthread_spinlock_t spinlock;
+  pthread_spin_init(&spinlock, 0);
 
-    // Function that removes items from the list in parallel
-    auto work = [&](){
-        while(true) {
-            // Grab the lock before doing anything
-            pthread_spin_lock(&spinlock);
-            
-            // Check if there are no more items
-            if (l.empty()) {
-                pthread_spin_unlock(&spinlock);
-                break;
-            }
+  // Function that removes items from the list in parallel
+  auto work = [&]() {
+    while (true) {
+      // Grab the lock before doing anything
+      pthread_spin_lock(&spinlock);
 
-            // Remove the item at the end of the list
-            l.pop_back();
-            pthread_spin_unlock(&spinlock);
-        }
-    };
+      // Check if there are no more items
+      if (l.empty()) {
+        pthread_spin_unlock(&spinlock);
+        break;
+      }
 
-    // Spawn threads
-    int num_threads = 8;
-    std::vector<std::jthread> threads;
-    for(int i = 0; i < num_threads; i++) {
-        threads.emplace_back(work);
+      // Remove the item at the end of the list
+      l.pop_back();
+      pthread_spin_unlock(&spinlock);
     }
-    return 0;
+  };
+
+  // Spawn threads
+  int num_threads = 8;
+  std::vector<std::jthread> threads;
+  for (int i = 0; i < num_threads; i++) {
+    threads.emplace_back(work);
+  }
+  return 0;
 }
